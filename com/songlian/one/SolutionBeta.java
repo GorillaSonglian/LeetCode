e4373f143d3975c2ae9feb2c638f46a8719f86cf
Songlian Li <songlianli@SongliandeMacBook-Pro.local>
2019-01-02 10:17:25 +0800

    add solution

---
+++ com/songlian/one/SolutionBeta.java
@@ -0,0 +1,33 @@
+package songlian.one;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class SolutionBeta {
+
+    public static void main(String[] args) {
+
+        SolutionBeta instance = new SolutionBeta();
+    }
+
+    // 7ms
+    // may be use binary search would be faster
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        int i = 0;
+        int curr;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        while (i < nums.length) {
+            curr = target - nums[i];
+            if (map.containsKey(curr)) {
+                result[0] = map.get(curr);
+                result[1] = i;
+                break;
+            }
+            map.put(nums[i], i++);
+        }
+        return result;
+    }
+}
