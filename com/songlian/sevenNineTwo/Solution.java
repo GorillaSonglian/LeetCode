47e947538a55852774ea3d2d8fd29acfb2eae497
GorillaSonglian <xiaopengyoumencaicaiwoshishui@gmail.com>
2019-01-14 11:57:36 +0800

    need to be faster

--- com/songlian/sevenNineTwo/Solution.java
+++ com/songlian/sevenNineTwo/Solution.java
@@ -2,15 +2,9 @@ package songlian.sevenNineTwo;
 
 public class Solution {
 
-    // This Solution is too slow!!!
+    // This Solution is too slow!!! 1258ms
     public static void main(String[] args) {
-
         Solution instance = new Solution();
-        String s = "rwpddkvbnnuglnagtvamxkqtwhqgwbqgfbvgkwyuqkdwhzudsxvjubjgloeofnpjqlkdsqvruvabjrikfwronbrdyyjnakstqjac";
-        String[] array =
-                {"wpddkvbnn","lnagtva","kvbnnuglnagtvamxkqtwhqgwbqgfbvgkwyuqkdwhzudsxvju","rwpddkvbnnugln","gloeofnpjqlkdsqvruvabjrikfwronbrdyyj","vbgeinupkvgmgxeaaiuiyojmoqkahwvbpwugdainxciedbdkos","mspuhbykmmumtveoighlcgpcapzczomshiblnvhjzqjlfkpina","rgmliajkiknongrofpugfgajedxicdhxinzjakwnifvxwlokip","fhepktaipapyrbylskxddypwmuuxyoivcewzrdwwlrlhqwzikq","qatithxifaaiwyszlkgoljzkkweqkjjzvymedvclfxwcezqebx"};
-        System.out.println(instance.numMatchingSubseq(s, array));
-
     }
 
     public int numMatchingSubseq(String S, String[] words) {
@@ -23,7 +17,7 @@ public class Solution {
 
     private boolean isSubstring(String S, String word) {
         int cur = -1;
-        int fromIndex = 0;
+        int fromIndex = -1;
         for (char c : word.toCharArray()) {
             int index = S.indexOf(c, fromIndex);
             if (index == -1) return false;
--- com/songlian/sevenNineTwo/SolutionBeta.java
+++ com/songlian/sevenNineTwo/SolutionBeta.java
@@ -2,13 +2,26 @@ package songlian.sevenNineTwo;
 
 public class SolutionBeta {
 
+    // Still too slow!!! 651ms
     public static void main(String[] args) {
 
         SolutionBeta instance = new SolutionBeta();
     }
 
     public int numMatchingSubseq(String S, String[] words) {
+        int count = 0;
+        for (String word : words) {
+            if (isSubstring(S, word)) count++;
+        }
+        return count;
+    }
 
-        return 0;
+    private boolean isSubstring(String S, String word) {
+        int index = -1;
+        for (char c : word.toCharArray()) {
+            index = S.indexOf(c, index + 1);
+            if (index == -1) return false;
+        }
+        return true;
     }
 }
