a53ffed91f9d492eb9740caf8cb2c1455c4e7e66
GorillaSonglian <xiaopengyoumencaicaiwoshishui@gmail.com>
2019-01-14 10:52:24 +0800

    need to add beta solution

---
+++ com/songlian/fourEightTwo/Solution.java
@@ -0,0 +1,14 @@
+package songlian.fourEightTwo;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Solution instance = new Solution();
+    }
+
+    public String licenseKeyFormatting(String S, int K) {
+        String result = "";
+        return result;
+    }
+}
---
+++ com/songlian/sevenNineTwo/Solution.java
@@ -0,0 +1,37 @@
+package songlian.sevenNineTwo;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Solution instance = new Solution();
+        String s = "rwpddkvbnnuglnagtvamxkqtwhqgwbqgfbvgkwyuqkdwhzudsxvjubjgloeofnpjqlkdsqvruvabjrikfwronbrdyyjnakstqjac";
+        String[] array =
+                {"wpddkvbnn","lnagtva","kvbnnuglnagtvamxkqtwhqgwbqgfbvgkwyuqkdwhzudsxvju","rwpddkvbnnugln","gloeofnpjqlkdsqvruvabjrikfwronbrdyyj","vbgeinupkvgmgxeaaiuiyojmoqkahwvbpwugdainxciedbdkos","mspuhbykmmumtveoighlcgpcapzczomshiblnvhjzqjlfkpina","rgmliajkiknongrofpugfgajedxicdhxinzjakwnifvxwlokip","fhepktaipapyrbylskxddypwmuuxyoivcewzrdwwlrlhqwzikq","qatithxifaaiwyszlkgoljzkkweqkjjzvymedvclfxwcezqebx"};
+        System.out.println(instance.numMatchingSubseq(s, array));
+
+    }
+
+    public int numMatchingSubseq(String S, String[] words) {
+        int count = 0;
+        for (String word : words) {
+            if (isSubstring(S, word)) count++;
+        }
+        return count;
+    }
+
+    private boolean isSubstring(String S, String word) {
+        int cur = -1;
+        int fromIndex = 0;
+        for (char c : word.toCharArray()) {
+            int index = S.indexOf(c, fromIndex);
+            if (index == -1) return false;
+            else if (index > cur) {
+                cur = index;
+                fromIndex = index + 1;
+            } else return false;
+        }
+        return true;
+    }
+
+}
