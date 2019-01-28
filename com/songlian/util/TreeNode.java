edb52f92c4f4cf3eed874c64958f4d361b4c0c08
GorillaSonglian <xiaopengyoumencaicaiwoshishui@gmail.com>
2019-01-28 11:00:18 +0800

    Fastest

---
+++ com/songlian/util/TreeNode.java
@@ -0,0 +1,13 @@
+package songlian.util;
+
+public class TreeNode {
+
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
